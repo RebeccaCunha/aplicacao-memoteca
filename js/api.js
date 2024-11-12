4acd15eaf98c27e4fa9b977d517bddf543ccf599
Rebecca Cunha <marquesrebecca94@gmail.com>
2024-11-12 12:05:07 -0300

    implementando CRUD com requisições HTTP

--- js/api.js
+++ js/api.js
@@ -1,11 +1,70 @@
+const URL_BASE = "http://localhost:3000"
+
 const api = {
-    async buscarPensamento () {
+    async buscarPensamentos () {
         try {
-            const response = await fetch('http://localhost:3000/pensamentos')
+            const response = await fetch(`${URL_BASE}/pensamentos`)
             return await response.json()
         }
         catch {
             alert('Erro ao buscar pensamentos')
+            throw error
+        }
+    },
+
+    async salvarPensamento (pensamento) {
+        try {
+            const response = await fetch(`${URL_BASE}/pensamentos`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        }
+        catch {
+            alert('Erro ao salvar pensamentos')
+            throw error
+        }
+    },
+
+    async buscarPensamentoPorId (id) {
+        try {
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`)
+            return await response.json()
+        }
+        catch {
+            alert('Erro ao buscar pensamentos')
+            throw error
+        }
+    },
+
+    async editarPensamento (pensamento) {
+        try {
+            const response = await fetch(`${URL_BASE}/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        }
+        catch {
+            alert('Erro ao editar pensamentos')
+            throw error
+        }
+    },
+
+    async excluirPensamento (id) {
+        try {
+            const response = await fetch(`${URL_BASE}/pensamentos/${id}`, {
+                method: "DELETE",
+            })
+        }
+        catch {
+            alert('Erro ao excluir pensamentos')
             throw error
         }
     }
